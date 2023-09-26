99c55e53daa1770df9a964cd204153ee5f69d638
Nathan VanSickle <ndvans03@gmail.com>
2023-09-26 17:22:56 -0400

    add basic methods

---
+++ ReadyQueue.py
@@ -0,0 +1,10 @@
+class ReadyQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(process):
+        self.queue.append(process)
+
+    def pop():
+        return self.queue.pop()
+    
