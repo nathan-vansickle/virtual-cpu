9627ca503a9277821dcc177f9d7474176f39f3cc
Nathan VanSickle <ndvans03@gmail.com>
2023-09-26 17:03:36 -0400

    define Process initialization

---
+++ Process.py
@@ -0,0 +1,7 @@
+class Process:
+    def __init__(self, duration, priority, arrival_time):
+        self.duration = duration
+        self.priority = priority
+        self.arrival_time = arrival_time
+
+    
